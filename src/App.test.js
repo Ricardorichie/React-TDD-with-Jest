3feff4fd1f1fa6e603d4a9420d4adf7b91e65a1b
Richard adesina <Richardadesina26@gmail.com>
2021-02-17 15:38:05 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+//  #use describe, it pattern
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(
+      getByText(/Getting started with React testing library/i)
+    ).toBeInTheDocument();
+  });
 });
